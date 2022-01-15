f6d27cf83e50b153e4ed292fa81ea4eb464956f1
monkey <monkey@monkeys-MBP.fritz.box>
2022-01-15 13:43:05 +0100

    adding some constructors and pretty prints

--- 8_onwards.rs
+++ 8_onwards.rs
@@ -1,19 +1,50 @@
+use std::fmt;
+
 // Flow Control
 // if-else: all conditions are branches and must return same type
 
-fn is_tall(h:i32){
+fn is_tall(h:i32) -> bool {
     if h > 190 {
         println!("person is tall");
+        return true;
     } else if h == 0 {
         println!("person is nonexistent");
+        return false;
     } else {
         println!("person is not tall");
+        return false;
+    }
+}
+
+// Methods, Functions
+struct Transaction {
+  sender: u32,
+  reciever: u32,
+  sats: i32
+}
+
+// Associated function that can be used as constructor, does not need to be called with an instance
+impl Transaction {
+  // Constructor 1
+  fn empty() -> Transaction {
+    Transaction { sender: 0, reciever: 0, sats: 0 }
+  }
+
+  fn new(s: u32, r: u32, sats: i32) -> Transaction {
+      Transaction { sender: s, reciever: r, sats: sats }
+  }
+}
+
+// adding pretty print for the struct
+impl fmt::Display for Transaction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Transaction published by {} to {} for {} sats", self.sender, self.reciever, self.sats)
     }
 }
 
 fn main() {
+    let _tr = Transaction::empty(); //intentional unused var gets underscore
+    let tr2 = Transaction::new(1, 2, 3000);
 
-    let h = 0;
-    let txt = String::from(is_tall(h))
-    println!(txt);
+    println!("{}", tr2.to_string());
 }
