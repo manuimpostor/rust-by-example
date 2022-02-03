d0aaa309294f72945ed37e6aa50efe355471f281
Manu Impostor <manu@impostor>
2022-02-03 10:08:45 +0100

    odd

--- tut2.rs
+++ tut2.rs
@@ -6,12 +6,12 @@ fn main(){
     let equal_lenth = |x, y| {
         x == y
     }; 
-    let odd_len = |x:i32| {
-        x.length % 2 == 0
+    let odd = |x:i32| {
+        x % 2 == 0
     };
 
     println!("array is {:?}", arr);
     println!("arrays same length {:?}", equal_lenth(arr, assumed_arr));
 
-    println!("odd one: {:?}", odd_len(arr));
+    println!("odd one: {:?}", odd(5));
 }
