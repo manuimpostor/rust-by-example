a9033d4570e1671e5e06e656ec3d0be3915aab76
Manu Impostor <manu@impostor>
2022-02-03 11:31:07 +0100

    nested mods and imports

--- tut2.rs
+++ tut2.rs
@@ -3,11 +3,25 @@
 // modules grouped together make a crate
 // public methods or modules need to be exposed actively
 
-pub mod lengther {
-    pub fn equal_lenth(x:&[i32], y:&[i32]) -> bool {
-        x == y
-    } 
+pub mod comp {
+    pub mod two {
+        pub mod ints {
+            pub fn if_odd(x:i32) -> bool {
+                x % 2 == 0
+            }
+        }
+    }
 }
+
+pub mod lengther {
+    pub mod arrayer {
+        pub fn equal_lenth(x:&[i32], y:&[i32]) -> bool {
+            x == y
+        }
+    }
+}
+
+use comp::two::ints::if_odd;
 fn main(){
     let arr:[i32;4] = [10, 20, 30, 40];
     let assumed_arr = [10, 20, 30, 40];
@@ -20,7 +34,8 @@ fn main(){
 
     println!("array is {:?}", arr);
     println!("arrays same length {:?}", equal_lenth(arr, assumed_arr));
-    println!("module::arrays same length {:?}", lengther::equal_lenth(&arr, &assumed_arr));
+    println!("module::arrays same length {:?}", lengther::arrayer::equal_lenth(&arr, &assumed_arr));
 
     println!("odd one: {:?}", odd(5));
+    println!("module: odd one: {:?}", if_odd(4));
 }
