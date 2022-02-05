b2fd19d09d7a0021b298d0f510fd9e99ab71a8a0
Manu Impostor <manu@impostor>
2022-02-05 19:26:46 +0100

    printing cities here

--- src/collections.rs
+++ src/collections.rs
@@ -13,10 +13,18 @@ pub mod collections {
     // TODO: import city names into a hashset
     // next: filter by city row, put that in hashset, call from main with few examples, renume fn
     fn read_cities() -> Result<(), Box<dyn Error>> {
-        let mut rdr = Reader::from_path("./src/cities.csv")?;
+        let mut rdr = Reader::from_path("./src/uspop.csv")?;
+        let headers = rdr.headers()?;
+        println!("{:?}", headers);
+
         for result in rdr.records() {
             let record = result?;
-            println!("{:?}", record);
+
+            let mut i = 0;
+            for r in record.into_iter() {
+                if i % 5 == 0 {println!("{:?}", r);}
+                i = i + 1;
+            }
         }
         Ok(())
     }
